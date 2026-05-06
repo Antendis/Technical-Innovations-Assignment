332ae8c895ca033cc2d2c97adc390f5a27a494fc
0wah0 <cai.perkins03@gmail.com>
2026-05-07 00:37:54 +0100

    Completion of user authentication and protected routes

--- server.js
+++ server.js
@@ -3,12 +3,17 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
+const authRoutes = require('./routes/auth');
+const authMiddleware = require('./middleware/authMiddleware');
+
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use('/api/auth', authRoutes);
 
 mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log ('MongoDB Connected'))
@@ -19,6 +24,15 @@ app.get('/', (req, res) => {
 });
 
 const PORT = process.env.port || 5000;
+
+app.get('/api/protected', authMiddleware, (req, res) => {
+
+    res.json({
+        message: 'Protected route accessed',
+        user: req.user
+    });
+
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
