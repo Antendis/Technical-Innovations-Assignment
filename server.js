65b303bb0c0c30093dc4f63668a1a840eb60f764
0wah0 <cai.perkins03@gmail.com>
2026-05-06 20:02:21 +0100

    Backend setup with MongoDB access

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log ('MongoDB Connected'))
+.catch(err => console.log(err))
+
+app.get('/', (req, res) => {
+    res.send('API is running')
+});
+
+const PORT = process.env.port || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
